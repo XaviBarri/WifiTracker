a84469af49f31f8791cdf86eb62f3aa1432d5398
Xavi Gonzalez Barri <xavi@xavi-laptop-Huawei>
2019-10-05 20:41:36 +0200

    Added main

--- WifiTracker.cpp
+++ WifiTracker.cpp
@@ -1,4 +1,15 @@
+//============================================================================
+// Name        : WifiTracker2.cpp
+// Author      : Xavi Gonzalez Barri
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
 
-int main(int argc, char **argv) {
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "In-Memory DataBase test" << endl; // prints In-Memory DataBase test
 	return 0;
 }
